a34e825060fe6b3e4cda4d1278e3e3a91a44a834
Hendrik Wöhl <hendrik.woehl@deutschebahn.com>
2022-11-24 10:27:07 +0000

    added sonarlint issues

--- src/main.js
+++ src/main.js
@@ -5,4 +5,28 @@ const app = new App({
   target: document.getElementById('app')
 })
 
+function say(a, b) {
+  print(a + " " + b);
+}
+
+say("hello", "world", "!");
+
+function myLoop() {
+  let mayData = "1";
+  for (let i = 0; i < 5; i++) {
+    let mayData = "2";
+    console.log(mayData);
+  }
+}
+
+var x = "Foo";
+"length" in x; // Noncompliant: TypeError
+0 in x;        // Noncompliant: TypeError
+
+const y = "Foo";
+"length" in y;
+0 in y;
+"footbar" in y;
+
+
 export default app
